65cf1c12df18fab7f4a059acf2a5e2a0d6f3233f
Julia Naomi <jnboeira@outlook.com>
2022-06-20 00:35:00 -0500

    first plugin

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_startup_system(setup_camera)
+        .add_plugin(HelloPlugin)
+        .add_plugins(DefaultPlugins)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn hello_plugin() {
+    println!("hello plugin!");
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(hello_plugin);
+    }
 }
