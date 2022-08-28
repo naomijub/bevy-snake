aeb4a92aae2c14885a8dd0015a996ae443c0baa7
Julia Naomi <jnboeira@outlook.com>
2022-08-28 12:32:32 -0500

    add-snake-cadency

--- src/components.rs
+++ src/components.rs
@@ -22,6 +22,25 @@ impl Size {
     }
 }
 
+#[derive(PartialEq, Debug, Copy, Clone)]
+pub enum Direction {
+    Left,
+    Up,
+    Right,
+    Down,
+}
+
+impl Direction {
+    pub fn opposite(self) -> Self {
+        match self {
+            Self::Left => Self::Right,
+            Self::Right => Self::Left,
+            Self::Up => Self::Down,
+            Self::Down => Self::Up,
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -35,5 +54,13 @@ mod test {
         let actual = Size::square(3.14);
 
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn opposite_direction() {
+        assert_eq!(Direction::Up.opposite(), Direction::Down);
+        assert_eq!(Direction::Down.opposite(), Direction::Up);
+        assert_eq!(Direction::Right.opposite(), Direction::Left);
+        assert_eq!(Direction::Left.opposite(), Direction::Right);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,17 @@ fn main() {
         .add_startup_system(setup_camera)
         .add_startup_system(snake::spawn_system)
         .add_plugins(DefaultPlugins)
-        .add_system(snake::movement_system)
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(1.0))
                 .with_system(food::spawn_system),
         )
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(0.150))
+                .with_system(snake::movement_system),
+        )
+        .add_system(snake::movement_input_system.before(snake::movement_system))
         .add_system_set_to_stage(
             CoreStage::PostUpdate,
             SystemSet::new()
